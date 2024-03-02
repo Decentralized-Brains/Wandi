7b4ea33fc90b89eeadb2ef9548aa163da6017a00
alfazsozib <alfazsozib.com>
2024-03-03 03:04:36 +0600

    home page done

--- src/components/global/Hero.jsx
+++ src/components/global/Hero.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Nav from "./Nav";
 import Bg from "../../assets/background/pearl.mp4";
+import { MdArrowOutward } from "react-icons/md";
+
 
 function Hero() {
   return (
     <div>
-      
       <div className="mainpage">
-        
         <div className="video-docker  fixed left-0 top-0 h-full w-full overflow-hidden">
           <video
             className="absolute min-h-full min-w-full object-cover "
@@ -19,19 +19,37 @@ function Hero() {
           ></video>
         </div>
         <Nav />
-
         <div className="hero-text-section container relative top-32 flex flex-col gap-10">
-            <div className="w-5/6">
-              <h1 className="text-[#CCCCCC] font-superLagendBoy text-[4.2rem]">
-              <span>BORROW</span> & <span> LEND </span>
-              AGAINST YOUR
-              NFT’s INSTANTLY
-              </h1>
-            </div>
-            <div className="buttons flex gap-10 font-superLagendBoy">
-              <button className="px-10 py-4 border border-white text-[#DBFF00] text-2xl">Borrow</button>
-              <button className="px-10 py-4 border border-[#DBFF00] text-2xl text-white bg-[#536223]">Lend</button>
-            </div>
+          <div className="w-5/6">
+            <h1 className="text-[#CCCCCC] font-superLagendBoy text-[4.2rem]">
+              <span className="text-gradient-bg">BORROW</span> &{" "}
+              <span className="text-gradient-bg"> LEND </span>
+              AGAINST YOUR NFT’s INSTANTLY
+            </h1>
+          </div>
+          <div className="buttons flex gap-10 font-superLagendBoy">
+            <button className="px-10 py-4 border border-white text-[#DBFF00] text-2xl">
+              Borrow
+            </button>
+            <button className="px-10 py-4 border border-[#DBFF00] text-2xl text-white bg-[#536223]">
+              Lend
+            </button>
+          </div>
+
+          <div className="box-section w-full backdrop-blur-2xl mt-12 p-8 rounded-lg ">
+              <div className="flex justify-between font-superLagendBoy">
+                <div className="flex flex-col gap-2">
+                  <h1 className="text-[#DBFF00] text-2xl">“DWØPE”</h1>
+                  <h1 className="text-white text-lg">RELEASED ON LUKSO, MARCH 2024</h1>
+                  <h1 className="text-white text-lg">PUBLIC LAUNCH IN APRIL</h1>
+                </div>
+
+                <div className="flex flex-col gap-2 justify-end items-end">
+                  <p className="text-white text-lg">Make money as a lender, <br /> get cash as a borrower.</p>
+                  <p className="text-[#DBFF00] flex gap-1 text-md items-center">Read the bitepaper <span><MdArrowOutward size={22}/></span></p>
+                </div>
+              </div>
+          </div>
         </div>
       </div>
     </div>
