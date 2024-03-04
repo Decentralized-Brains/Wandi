0ff4539139da20b1d193edc2eb58b65a67c8f55f
alfazsozib <alfazsozib.com>
2024-03-05 01:41:09 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import Home from "./components/Home/Home";
 import Lend from "./components/Lend/Lend";
-import Hero from "./components/global/Hero";
-import Nav from "./components/global/Nav";
+import Offers from "./components/offers/Offers";
+import { Route, Routes } from "react-router-dom"
+
 
 function App() {
   return (
     <div className="App">
-      <Hero />
-      {/* <Lend/> */}
+      <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/lend" element={<Lend />} />
+      <Route path="/offers" element={<Offers />} />
+      </Routes>
     </div>
   );
 }
