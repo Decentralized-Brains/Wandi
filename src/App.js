12f1f0593c1d84a4c62060b818e6092cbaca2bd0
alfazsozib <alfazsozib.com>
2024-03-07 02:59:25 +0600

    done

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <Route path="/" element={<Home/>} />
       <Route path="/lend" element={<Lend />} />
       <Route path="/offers" element={<Offers />} />
-      <Route path="/loans" element={<Loans />} />
+      <Route path="/loans" element={<Loans/>} />
       <Route path="/borrow" element={<Borrow />} />
       </Routes>
     </div>
