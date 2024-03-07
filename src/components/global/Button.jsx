f21cadee8811f8a8ddb19c08e4cf660c20495dc9
alfazsozib <alfazsozib.com>
2024-03-07 22:44:19 +0600

    button fixed

--- src/components/global/Button.jsx
+++ src/components/global/Button.jsx
@@ -8,7 +8,7 @@ function Button({btnText}) {
       <div style={{ position: "relative", width: "fit-content" }}>
         {/* <button className="button-style px-6">Select Profile</button> */}
        <button
-          className="relative button-style border border-[#DBFF00] px-4 py-2 bor"
+          className="relative button-style bg-gradient-to-t from-[#c6e3076e] to-[#ddff0000] border border-[#DBFF00] px-4 py-2 bor"
           style={{ position: "relative", overflow: "hidden" }}
         > 
           <span className="text-[#DBFF00] z-[10] relative">{btnText}</span> 
